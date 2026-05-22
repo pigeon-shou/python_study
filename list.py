1b117f74df270fbd6fadce9bc704739afdc7961e
pigeon-shou <pigeonshow2024@gmail.com>
2026-05-22 14:04:29 +0900

    list
    配列に近い
    appendでruby でいう>>追加
    -1で配列の後ろから取れる

---
+++ list.py
@@ -0,0 +1,19 @@
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+fruits = ["grape", "orange", "strawberry"]
+print(fruits[0])
+
+fruits = ["strawberry", "banana", "apple"]
+print(fruits[-1])
+
+fruits.append("orange")
+print(fruits)
+print(len(fruits))
+
+fruit = input("果物を入力してください")
+fruits = ["apple", "orange", "strawberry"]
+
+
+fruits.append("fruit")
+print(fruits)
