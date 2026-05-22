a47b88ea4e234835a549c841b5012c564934c43b
pigeon-shou <pigeonshow2024@gmail.com>
2026-05-22 12:53:58 +0900

    数値　文字列　数値　len str
    strは文字列に変換　rubyにおけるgets to_sのようなもの len strが重なった時の入れ子構造に注意

---
+++ string.py
@@ -0,0 +1,39 @@
+# 文字列の取り扱い
+print("Hello こんにちは！")
+print("Hello" + " こんにちは！")
+print("hello python3")
+print("hello" + " " + "python")
+print(6 + 8)
+
+# 数値、変数、文字列、python的式展開#{}
+name = "tanaka"
+age = 35
+print(name)
+print(age)
+print(name + "は" + str(age) + "才です")
+print(f"僕は{name}といいます。{age}才です")
+print(f"{age}")
+
+# 数値計算
+a = 13
+b = 11
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(b // a)
+
+# len
+print(len("python"))
+length = len("Hello World")
+print (length)
+name = "yamaguchi"
+print(len(name))
+num = 179
+print(len(str(num)))
+numbers = [1, 2, 3, 4]
+print(len(numbers))
+word = "ChatGPT"
+print(len(word))
